cdf068e66e544986581a1904e133fdf4997bd318
evstraub <evstraub@gmail.com>
2023-04-28 12:04:50 -0400

    routed the homepage using express

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000
 mongoose.connect(process.env.DB_URI, {useNewUrlParser: true} )
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
-db.once('open', () => console.log("connected to the database!!"))
+db.once('open', () => console.log("connected to the database"))
 
 //middlewares
 app.use(express.urlencoded({extended: false}))
