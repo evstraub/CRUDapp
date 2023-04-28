a8a20dee6ea2efcc8722f84c3b45042ae6c6b8d8
evstraub <evstraub@gmail.com>
2023-04-28 14:43:31 -0400

    added rout to new user form and styled form

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) =>{
     res.render('index', {title: "Home"})
 })
 
+router.get('/add', (req, res) =>{
+    res.render('add_users', {title: "Add Users"})
+})
+
 module.exports = router
