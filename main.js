61e99f1133bf2a07ee7361fedaa8c2df468cdd8e
evstraub <evstraub@gmail.com>
2023-04-28 10:28:39 -0400

    added a placefor routes

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ app.use((req, res, next) => {
 
 //set template engine
 app.set('view engine', "ejs")
-app.get('/', (req, res) =>{
-    res.send('Hello')
-})
+
+//route prefix 
+app.use('', require('./routes/routes'))
 
 app.listen(PORT, () => {
     console.log(` server started at http://localhost:${PORT}`)
