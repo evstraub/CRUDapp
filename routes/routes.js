cb57bfe09ac7249fbe9b3dad44bdcc1e0d119e43
evstraub <evstraub@gmail.com>
2023-04-30 20:10:16 -0400

    submitted user info to db

--- routes/routes.js
+++ routes/routes.js
@@ -2,14 +2,45 @@ const express = require('express')
 const router = express.Router()
 const User =require('../models/users')
 const multer = require('multer')
+const session = require('express-session')
 
 //image upload 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null,'./uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname)
     }
 })
 
+const upload = multer({
+    storage: storage,
+}).single('image')
+
+//insert a user into database
+router.post('/add', upload, (req, res) => {
+    const user = new User({
+       name: req.body.name, 
+       email: req.body.email, 
+       phone: req.body.phone,
+       image: req.file.filename, 
+    })
+   
+    user.save().then(() =>{
+       req.session.message ={
+        type: "success",
+        message: "user added successfully!"
+       }
+        res.redirect("/");
+       
+        
+    }).catch((err)=>{
+        console.log(err);
+    })
+});
+   
+   
 router.get('/', (req, res) =>{
     res.render('index', {title: "Home"})
 })
