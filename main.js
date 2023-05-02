e2a99969a0be9737e91b72a776abb49d9ee01495
evstraub <evstraub@gmail.com>
2023-05-02 10:32:33 -0400

    style changes

--- main.js
+++ main.js
@@ -22,10 +22,11 @@ app.use(session({
     secret: 'my secret key',
     saveUninitialized: true,
     resave: false,
-}))
+})
+)
 
 app.use((req, res, next) => {
-    res.locals.message = req.sessionID.message
+    res.locals.message = req.session.message
     delete req.session.message
     next()
 })
